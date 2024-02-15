42c2da5c7b2e0400b447fd5464cca9a827783b85
lulong <lulongyoyo@gmail.com>
2024-02-15 18:52:35 +0800

    add

---
+++ pon前端初階class/func.js
@@ -0,0 +1,16 @@
+function add(x,y){
+    console.log(x+y)
+}
+function multiply(x,y){
+    console.log(x*y)
+}
+/*
+export default{
+    ad:add,
+    mul:multiply
+}
+*/
+
+export default[
+    add,multiply
+]
---
+++ pon前端初階class/main.js
@@ -0,0 +1,13 @@
+import math from "./func.js"
+
+/*
+
+
+
+math.ad(1,2)
+math.mul(3,4)
+
+*/
+
+math[0](1,2)
+math[1](1,2)
